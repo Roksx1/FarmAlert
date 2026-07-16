92be7077800d81e304b4617fc5325a5e795f2d53
Laze <ryzzers69@gmail.com>
2026-07-16 23:20:50 +0530

    docs: add simple comments

--- src/main/java/net/roks/farmalert/config/FarmAlertConfig.java
+++ src/main/java/net/roks/farmalert/config/FarmAlertConfig.java
@@ -1,19 +1,18 @@
 package net.roks.farmalert.config;
 
+/**
+ * Global configuration layout for the FarmAlert mod.
+ */
 public class FarmAlertConfig {
 
+    // Config sections
     public EdgeConfig edge = new EdgeConfig();
     public TeleportConfig teleport = new TeleportConfig();
 
-    /*
-     * Future milestones
-     */
+    // Debug and UI properties
     public boolean debugMode = false;
-
     public boolean playSound = true;
-
     public int titleDuration = 40;
-
     public float titleScale = 1.0F;
 
 }
